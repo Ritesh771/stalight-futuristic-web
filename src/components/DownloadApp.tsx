93794dd9cee5025960c8fb033a89af583edc5969
Ritesh771 <riteshnvisonex@gmail.com>
2025-06-14 21:25:59 +0530

    Update download link in DownloadApp component

--- src/components/DownloadApp.tsx
+++ src/components/DownloadApp.tsx
@@ -88,7 +88,7 @@ const DownloadApp = () => {
 
             <div className="flex flex-col sm:flex-row gap-4 justify-center">
               <a
-                href="https://drive.google.com/uc?export=download&id=13AkUQSOv3AUR3BkjsHYuuxIiGwoUp6B9"
+                href="https://drive.google.com/uc?export=download&id=1QX9BTHvKMv4FCsxODy10WlWrFjV3ntQe"
                 className="inline-flex items-center justify-center px-8 py-4 bg-purple-600 hover:bg-purple-700 text-white rounded-xl transition-colors duration-300"
                 download
                 target="_blank"
